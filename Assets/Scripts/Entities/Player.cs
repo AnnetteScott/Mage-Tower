f6fdf5329eb3cf3731f0512c97e5b4a3add44841
AnnetteScott <notnatural21@gmail.com>
2024-04-17 11:58:36 +1200

    Added the ability to run

--- Assets/Scripts/Entities/Player.cs
+++ Assets/Scripts/Entities/Player.cs
@@ -6,8 +6,10 @@ using UnityEngine.InputSystem;
 public class Player : Entity
 {
     public InputAction move;
+    public InputAction run;
     public float jumpHeight;
     public float walkingSpeed;
+    public float runningSpeed;
     private Rigidbody2D rigidBody;
     private bool onGround;
 
@@ -16,6 +18,7 @@ public class Player : Entity
         init();
         onGround = true;
         move.Enable();
+        run.Enable();
         rigidBody = GetComponent<Rigidbody2D>();
         rigidBody.freezeRotation = true;
 
@@ -23,6 +26,18 @@ public class Player : Entity
 
     void Update()
     {
+        movePlayer();
+    }
+
+    private void movePlayer()
+    {
+        float speed = walkingSpeed;
+        if (run.IsPressed())
+        {
+            speed = runningSpeed;
+        }
+
+
         if (move.IsPressed())
         {
             Vector2 moveValue = move.ReadValue<Vector2>();
@@ -30,14 +45,14 @@ public class Player : Entity
             if (onGround && moveValue.y != 0.0f)
             {
                 onGround = false;
-                rigidBody.velocity = new Vector2(moveValue.x * walkingSpeed, jumpHeight);
+                rigidBody.velocity = new Vector2(moveValue.x * speed, jumpHeight);
             }
             else
             {
-                rigidBody.velocity = new Vector2(moveValue.x * walkingSpeed, rigidBody.velocity.y);
+                rigidBody.velocity = new Vector2(moveValue.x * speed, rigidBody.velocity.y);
             }
 
-            if(moveValue.x != 0.0f)
+            if (moveValue.x != 0.0f)
             {
                 Vector3 localScale = transform.localScale;
                 localScale.x = moveValue.x < 0.0f ? -1 : 1;
@@ -45,10 +60,11 @@ public class Player : Entity
             }
 
         }
-        else if(onGround)
+        else if (onGround)
         {
             rigidBody.velocity = new Vector2(0, 0);
-        }else
+        }
+        else
         {
             rigidBody.velocity = new Vector2(0, rigidBody.velocity.y);
         }
