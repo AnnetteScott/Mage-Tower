4cec9af18da5a7bac708982b28b1f29598e55f13
bubbltea <ethangotfrid1@gmail.com>
2024-05-03 13:03:22 +1200

    Allow Main Menu play button to run game

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
@@ -8,7 +9,7 @@ public class MainMenu : MonoBehaviour
     public void PlayGame ()
     {
         //Need to add "Game" scene to build settings in unity after the 'MainMenu' for this to function, waiting on game creation
-        //SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
 
     public void QuitGame ()
