2bdbbc520cc6ec9a983d87b657ca51da35c1f75a
AnnetteScott <notnatural21@gmail.com>
2024-05-20 16:45:17 +1200

    Enable equipped modifiers to do more damage

--- Assets/Scripts/Entities/Bullet.cs
+++ Assets/Scripts/Entities/Bullet.cs
@@ -35,6 +35,21 @@ public class Bullet : MonoBehaviour
         }
     }
 
+    public float getDamage()
+    {
+        float damageModifier = 1;
+
+        if (GlobalData.equippedStaffItem != null)
+        {
+            var resource = Resources.Load(GlobalData.equippedStaffItem);
+            GameObject newInstance = Instantiate(resource) as GameObject;
+            damageModifier = newInstance.GetComponent<Weapon>().power;
+            Destroy(newInstance);
+        }
+
+        return damage * damageModifier;
+    }
+
     private void OnTriggerStay2D(Collider2D collision)
     {
         if (collision.gameObject.CompareTag("Ground"))
